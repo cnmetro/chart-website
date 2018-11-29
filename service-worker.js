d726a4de299d05b1eab5dfcf49609fae8daa2a7a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-29 03:49:50 +0000

    Deploy cnmetro/chart-website to github.com/cnmetro/chart-website.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5921a1af12d6459ca081efd77e8ab87.js"
+  "/precache-manifest.8895c3aa252f85274d508af74bed47bb.js"
 );
 
 workbox.clientsClaim();
