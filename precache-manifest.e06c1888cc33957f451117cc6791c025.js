b28ca06265dd29cfeb6713991faae2e484153eec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-06 02:57:29 +0000

    Deploy cnmetro/chart-website to github.com/cnmetro/chart-website.git:gh-pages

---
+++ precache-manifest.e06c1888cc33957f451117cc6791c025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a880c8b853e082d5ae8e5552c0f4e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2610a0d7a4de04d740a8",
+    "url": "/static/css/main.9478c8ab.chunk.css"
+  },
+  {
+    "revision": "74a1c9b2e5b547679025",
+    "url": "/static/js/2.e7c0764c.chunk.js"
+  },
+  {
+    "revision": "5810eb8b77ca213afa928c0d500f691b",
+    "url": "/static/js/2.e7c0764c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2610a0d7a4de04d740a8",
+    "url": "/static/js/main.c4474eef.chunk.js"
+  },
+  {
+    "revision": "9dffdae389ecd168e8fb",
+    "url": "/static/js/runtime-main.d5c34905.js"
+  },
+  {
+    "revision": "cd5e6a7c580c7a628d566a24377e988d",
+    "url": "/static/media/logo.cd5e6a7c.svg"
+  }
+]);
